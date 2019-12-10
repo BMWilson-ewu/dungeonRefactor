88ae1dc43fb5f3d4744c4e5afcb854a234a47ef6
entz95 <jentz6@protonmail.com>
2019-12-09 22:26:01 -0800

    in process of moving main from dungeon to DungeonAdventure

--- DungeonRefactor/src/specials/TwinStrike.java
+++ DungeonRefactor/src/specials/TwinStrike.java
@@ -1,19 +1,20 @@
 package specials;
 import entities.DungeonCharacter;
+import weapons.AttackPool;
 
 public class TwinStrike implements SpecialAbility{
 	public void special(DungeonCharacter source, DungeonCharacter enemy) {
 		double two = Math.random();
 		if (two <= .4) {
 			System.out.println(source.getName() + " hits twice!");
-			source.attack(enemy);
-			source.attack(enemy);
+			AttackPool.getInstanceOf().getbasicAttack().attack(source, enemy);
+			AttackPool.getInstanceOf().getbasicAttack().attack(source, enemy);
 		} // end surprise
 		else if (two >= .8) {
 			System.out.println("Oh no! " + source.getName() + " missed both attacks!");
 		} else {
 			System.out.println(source.getName() + " hits once!");
-			source.attack(enemy);
+			AttackPool.getInstanceOf().getbasicAttack().attack(source, enemy);
 		}
 			
 	}
